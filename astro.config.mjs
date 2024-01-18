359ac27bf40bba6ff6279bc9d113759dd731f5bf
Lex Miin <alexmiyin@gmail.com>
2024-01-18 16:27:53 +0200

    feat: add storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), "STORYBLOK")
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(),
+    react(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        'page': 'storyblok/Page',
+        'marina': 'storyblok/Item',
+        'marinaList': 'storyblok/MarinaList'
+      },
+      apiOptions: {
+        region: 'eu'
+      }
+    })
+  ]
 });
