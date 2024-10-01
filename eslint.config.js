16a5a2e73d6098660dca5fe20857afb828d40db5
Lex Miin <alexmiyin@gmail.com>
2024-10-01 13:14:41 +0300

    revert: seems unnecessary
    This reverts commit 08167207c8a6fbaeb7c2dbd1f79a6ab5e2a38588.

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import eslintPluginAstro from 'eslint-plugin-astro'
-import tseslint from 'typescript-eslint'
 
 export default [
   ...eslintPluginAstro.configs.recommended,
-  ...tseslint.configs.recommended
+  {
+    rules: {}
+  }
 ]
